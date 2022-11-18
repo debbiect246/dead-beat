04012beb4cabf19fa825e58e564d4abaedd1bddd
kelvinhere <kwardeu@gmail.com>
2022-11-18 12:20:26 +0000

    FEAT: Remove missed beats that go off screen

--- game.js
+++ game.js
@@ -24,6 +24,7 @@ var beatsPerMin = 40;  // Set this to the BPM of the song
 var beatsPerMillisecond = 60000 / beatsPerMin;
 var notesPerBeat = 4; // Default 4/4 measure
 var beatsPerBar = beatsPerMillisecond / notesPerBeat;
+var beatsGroup;
 
 function preload () {
     this.load.image('sky', './assets/images/sky.png');
@@ -34,8 +35,13 @@ function preload () {
 function create () {
     // Sky image (added as tileSprite to access the scrolling method)
     sky = this.add.tileSprite(400, 300, 800, 600, 'sky');
+
+    // Create the group for beats
+    beatsGroup = this.add.group();
+
     // Create a timed event that will spawn an object for each note, defined by BPM and notesPerBeat
-    timedEvent = this.time.addEvent({ delay: beatsPerBar, callback: beatSpawn, callbackScope: this, loop: true });
+    beatSpawnerEvent = this.time.addEvent({ delay: beatsPerBar, callback: beatSpawn, callbackScope: this, loop: true });
+    beatRemoverEvent = this.time.addEvent({ delay: 1000, callback: beatRemover, callbackScope: this, loop: true });
     
 }
 
@@ -51,5 +57,14 @@ function beatSpawn() {
     var beat = this.physics.add.sprite(100, 700,'beat');
     beat.setMaxVelocity(0 , 250);
     beat.setVelocityY(-250);
+    beatsGroup.add(beat);
+}
+
+function beatRemover() {
+    beatsGroup.children.each(function(beat) {
+        if (beat.x < -100) {
+            beat.destroy();
+        };
+    });
 }
 ;
